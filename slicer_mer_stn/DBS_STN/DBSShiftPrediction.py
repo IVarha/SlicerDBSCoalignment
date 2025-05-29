f599a83a085278e9de268a50db0173c099a6e6ba
Igor Varga <ivarhauzh@gmail.com>
2025-05-29 17:44:52 +0200

    Commit Example Scene

--- slicer_mer_stn/DBS_STN/DBSShiftPrediction.py
+++ slicer_mer_stn/DBS_STN/DBSShiftPrediction.py
@@ -554,33 +554,21 @@ def registerSampleData():
     SampleData.SampleDataLogic.registerCustomSampleDataSource(
         # Category and sample name displayed in Sample Data module
         category='DBSShiftPrediction',
-        sampleName='DBSShiftPrediction1',
+        sampleName='OR Scene',
         # Thumbnail should have size of approximately 260x280 pixels and stored in Resources/Icons folder.
         # It can be created by Screen Capture module, "Capture all views" option enabled, "Number of images" set to "Single".
-        thumbnailFileName=os.path.join(iconsPath, 'DBSShiftPrediction1.png'),
+        thumbnailFileName=os.path.join(iconsPath, 'coalignment_screenshot.png'),
         # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95",
-        fileNames='DBSShiftPrediction1.nrrd',
+        uris="https://github.com/IVarha/SlicerDBSCoalignment/releases/download/0.0.1/left_share.mrb",
+        fileNames='left_shared.mrb',
+        loadFiles=True,  # This is a MRB file, so it will be loaded into the scene
+        loadFileType='SceneFile',  # This is a scene file, so it will be loaded into the scene
         # Checksum to ensure file integrity. Can be computed by this command:
         #  import hashlib; #print(hashlib.sha256(open(filename, "rb").read()).hexdigest())
-        checksums='SHA256:998cb522173839c78657f4bc0ea907cea09fd04e44601f17c82ea27927937b95',
         # This node name will be used when the data set is loaded
         nodeNames='DBSShiftPrediction1'
     )
 
-    # DBSShiftPrediction2
-    SampleData.SampleDataLogic.registerCustomSampleDataSource(
-        # Category and sample name displayed in Sample Data module
-        category='DBSShiftPrediction',
-        sampleName='DBSShiftPrediction2',
-        thumbnailFileName=os.path.join(iconsPath, 'DBSShiftPrediction2.png'),
-        # Download URL and target file name
-        uris="https://github.com/Slicer/SlicerTestingData/releases/download/SHA256/1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97",
-        fileNames='DBSShiftPrediction2.nrrd',
-        checksums='SHA256:1a64f3f422eb3d1c9b093d1a18da354b13bcf307907c66317e2463ee530b7a97',
-        # This node name will be used when the data set is loaded
-        nodeNames='DBSShiftPrediction2'
-    )
 
 ########optimisers
 
